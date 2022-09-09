29b1a209a14b1efefe961183599ad6f3dcbba32c
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-09 10:12:37 +0200

    Adding new functionnalities + correcting old ones

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "thor"
+require 'thor'
 require_relative 'wrapper'
 
 # Class for the command line interface
@@ -16,21 +16,46 @@ class ScwOsp < Thor
 
     > $ main.rb get --name example-instance
 
-    > $ main.rb get --id <SCW ID>
-
     > $ main.rb get --private-ip <SCW PRIVATE IP>
 
-    > $ main.rb get --public-ip <SCW PUBLIC IP>
+    > $ main.rb get --state <SCW STATE>
+
+    > $ main.rb get --name example-instance --private-ip <SCW PRIVATE IP>
+
   LONGDESC
 
-  option :name, aliases: "-n"
-  option :private_ip, aliases: "-p"
+  option :name, aliases: '-n', type: :string, desc: 'Name of the instance'
+  option :state, aliases: '-s', type: :string, desc: 'State of the instance'
+  option :private_ip, aliases: '-pip', type: :string, desc: 'Private IP of the instance'
 
   def get
     if options.empty?
       Wrapper.new.list
     else
       Wrapper.new.get options
+    end
+  end
+
+  desc 'checkup', 'Check if any server is down and return the list of those who are'
+  def checkup
+    Wrapper.new.checkup
+  end
+
+  desc 'details', 'Get details of a server using given option'
+  long_desc <<-LONGDESC
+    Returns details for the given instance
+
+    > $ main.rb details --id <SCW ID>
+
+  LONGDESC
+
+  option :id, aliases: '-i', type: :string, desc: 'ID of the instance'
+
+  def details
+    if options.empty?
+      Wrapper.new.list
+    else
+      Wrapper.new.server_details options
     end
   end
 end
--- wrapper.rb
+++ wrapper.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
+require 'colorize'
 
-class IncorrectParameter < StandardError
-  def initialize(msg = "Error: Incorrect parameter format, please make a parameter that follows KEY:VALUE")
+# Class for the command line error interface
+class NoResultError < StandardError
+  def initialize(msg = 'Error: No Results Found')
     super
   end
 end
+
+# Class for the command line error interface
 class InvalidKey < StandardError
-  def initialize(msg = "Error: Incorrect parameter selected, it must be ID, IP or NAME")
+  def initialize(msg = 'Error: Incorrect parameter selected, it must be an ID')
     super
   end
 end
+
 # Class for the Wrapper that will give us our data
 class Wrapper
   def initialize
@@ -23,25 +28,42 @@ class Wrapper
   end
 
   def get(options)
-    begin
-      # raise IncorrectParameter unless param.include?(':')
-      # key, value = param.downcase.split(':')
-
-      # TODO: Be able to give multiple options
-      search = "#{options.keys.first}=#{options[options.keys.first]}"
-
-      instance = `scw instance server list #{search}`
-      if instance.empty?
-        puts 'No results found'
-      else
-        puts instance
-      end
-    rescue IncorrectParameter => e
-      # output: red
-      puts "[#{e.class}] - #{e.message}"
-    rescue InvalidKey => e
-      # output: yellow
-      puts "[#{e.class}] - #{e.message}"
+    search = ''
+    options.each do |option|
+      search += "#{option[0].gsub('_', '-')}=#{option[1]} "
     end
+
+    puts search
+    instance = `scw instance server list #{search}`
+    # If instance contains only letters, it means that the search didn't return any results
+    raise NoResultError unless instance.match?(/\d+/)
+
+    puts instance.colorize(:green)
+  rescue NoResultError => e
+    puts "[#{e.class}] - #{e.message}".colorize(:red)
+  end
+
+  def checkup
+    instance = `scw instance server list state=stopped`
+    if instance.match?(/stopped/)
+      puts instance.colorize(:red)
+    else
+      puts 'Every instance is running !'.colorize(:green)
+    end
+  end
+
+  def server_details(options)
+    # Get the id of the options
+    raise InvalidKey unless options.key?('id')
+
+    id = options[options.keys.first]
+    instance = `scw instance server get #{id}`
+    if instance.match?(/\d+/)
+      puts instance
+    else
+      puts 'No results found'.colorize(:red)
+    end
+  rescue InvalidKey => e
+    puts "[#{e.class}] - #{e.message}".colorize(:red)
   end
 end
